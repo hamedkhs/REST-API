e02f04223a60e11ef8f81bad9d7439ed9ba13b0b
hamed_khorshidi <hamedkhorshidi3497@gmail.com>
2024-09-03 12:06:43 +0330

    hamedkhorshidi2

--- demo/src/main/java/com/example/demo/LoadDatabace.java
+++ demo/src/main/java/com/example/demo/LoadDatabace.java
@@ -3,7 +3,7 @@
 ////*برای مقدار دهی اولیه ی پایگاه داده از این کلاس استفاده میشود**/
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
-//import org.springframework.boot.CommandLineRunner;
+///import org.springframework.boot.CommandLineRunner;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //
